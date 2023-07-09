609722f12206af376406757af4bdf41b22d58af2
Tojeaux <KT.Nguyen414@gmail.com>
2023-07-09 01:18:34 -0700

    Enhanced Finger movements

--- Assets/Code/Path.cs
+++ Assets/Code/Path.cs
@@ -2,26 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paths : MonoBehaviour
+public class Path : MonoBehaviour
 {
     public GameObject obj;
     public GameObject[] pathPoints;
     public int numberofPoints;
     public float speed;
 
-    private Vector3 actualPosition;
-    private int x;
-    private bool reverse; //set x to 0
+    public Vector3 actualPosition;
+    public int x;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         x = 1;
-        reverse = false;
+        
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
         actualPosition = obj.transform.position;
         obj.transform.position = Vector3.MoveTowards(actualPosition, pathPoints[x].transform.position, speed * Time.deltaTime);
